366c8a21f7bde6460c45792d72c8bddf36d3bf45
gonzalo <gonzalojvarisco@gmail.com>
2019-03-19 15:40:40 -0300

    avances

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,17 +1,38 @@
 const express = require('express');
 const app = express();
+const Usuario = require('../models/usuario');
+const bcrypt = require('bcrypt');
 
 app.get('/usuario', function (req, res) {
     res.json('getUsuario local');
   });
   
   app.post('/usuario', (req, res)=>{
-  
-      let body = req.body;
-  
-      res.json({
-          persona: body
-      });
+    
+    let body = req.body;
+
+        let usuario = new Usuario({
+            nombre:body.nombre,
+            email:body.email,
+            password: bcrypt.hashSync(body.password, 10),
+            role:body.role
+    });
+
+    usuario.save((err, usuarioDB)=>{
+        if(err){
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+           }
+        );
+    })
+
   });
   
   app.put('/usuario/:id', (req, res)=>{
