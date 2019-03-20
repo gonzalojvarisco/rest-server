55e21fa63401bc448c18ce10df6c656593f42ac8
gonzalo <gonzalojvarisco@gmail.com>
2019-03-20 15:46:13 -0300

    avances

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -2,9 +2,40 @@ const express = require('express');
 const app = express();
 const Usuario = require('../models/usuario');
 const bcrypt = require('bcrypt');
+const _ = require('underscore');
 
 app.get('/usuario', function (req, res) {
-    res.json('getUsuario local');
+
+    let desde = req.query.desde || 0;
+    desde = Number(desde);
+
+    let limite = req.query.limite || 5;
+    limite = Number(limite);
+
+    Usuario.find({}, 'nombre email role estado google img')
+        .skip(desde)
+        .limit(limite)
+        .exec((err, usuarios)=>{
+
+            if(err){
+                return res.status(400).json({
+                    ok: false,
+                    err
+                });
+            }
+
+            Usuario.count({}, (err, conteo)=>{
+                
+                res.json({
+                ok: true,
+                usuarios,
+                cantidad: conteo
+            
+                });
+            })
+
+            
+        })
   });
   
   app.post('/usuario', (req, res)=>{
@@ -37,8 +68,24 @@ app.get('/usuario', function (req, res) {
   
   app.put('/usuario/:id', (req, res)=>{
       let id = req.params.id;
+      let body = _.pick( req.body, ['nombre', 'email', 'img', 'role', 'estado']);
+
+      Usuario.findByIdAndUpdate(id, body, {new: true, runValidators: true}, (err, usuarioDB)=>{
+
+        if(err){
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        });
+
+      })
   
-      res.json({id});
   });
   
   app.delete('/usuario', (req, res)=>{
