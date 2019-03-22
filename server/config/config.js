f47a16dd2aeef21854582b074c328023b834ffe9
Gonzalo Varisco <gonzalojvarisco@gmail.com>
2019-03-22 00:30:36 -0300

    variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -9,7 +9,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://gonzalo:sXk2oF7A3sdh9OLD@cluster0-uxv2k.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 
 }
 
