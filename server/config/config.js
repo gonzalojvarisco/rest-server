a53fd853685186c816c577e8e6e769055e3d788a
gonzalo <gonzalojvarisco@gmail.com>
2019-03-22 13:24:07 -0300

    avances

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,13 @@
-process.env.PORT = process.env.PORT || 3000; //puerto
+//========================================
+//puerto
+//========================================
+process.env.PORT = process.env.PORT || 3000;
 
 
+
+//========================================
 //entorno
+//========================================
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 let urlDB;
@@ -9,8 +15,24 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = process.env.MONGO_URI;
+    urlDB = process.env.MONGO_URI; //MONGO_URI variable definida en heroku
 
 }
+
+//========================================
+//caducidad
+//========================================
+//60 segundos, 60 minutos, 24 horas, 30 dias
+
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+
+//========================================
+//seed de desarrollo
+//========================================
+
+process.env.SEED = 'seed-de-desarrollo';
+
+
 
 process.env.urlDB = urlDB;
