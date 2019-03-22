6b1694d9fdcaab4280932537e8ef500d3c30c6d3
Gonzalo Varisco <gonzalojvarisco@gmail.com>
2019-03-21 22:44:13 -0300

    restServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,4 @@
-
-process.env.PORT = process.env.PORT || 3000;  //puerto
+process.env.PORT = process.env.PORT || 3000; //puerto
 
 
 //entorno
@@ -7,11 +6,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 let urlDB;
 
-if(process.env.NODE_ENV === 'dev'){
+if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
-}else{
-    urlDB = 'mongodb+srv://gonzalo:sXk2oF7A3sdh9OLD@cluster0-uxv2k.mongodb.net/cafe'; 
-    
+} else {
+    urlDB = 'mongodb+srv://gonzalo:sXk2oF7A3sdh9OLD@cluster0-uxv2k.mongodb.net/cafe';
+
 }
 
 process.env.urlDB = urlDB;
