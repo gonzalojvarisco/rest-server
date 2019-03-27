b738a5b13d0199b429effc4a3da93b2991237c07
Gonzalo Varisco <gonzalojvarisco@gmail.com>
2019-03-27 13:26:46 -0300

    Login

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -3,8 +3,9 @@ const app = express();
 const Usuario = require('../models/usuario');
 const bcrypt = require('bcrypt');
 const _ = require('underscore');
+const { verificaToken, verificaAdmin_Role } = require('../middlewares/autenticacion');
 
-app.get('/usuario', function (req, res) {
+app.get('/usuario', verificaToken, (req, res) => {
 
     let desde = req.query.desde || 0;
     desde = Number(desde);
@@ -12,45 +13,45 @@ app.get('/usuario', function (req, res) {
     let limite = req.query.limite || 5;
     limite = Number(limite);
 
-    Usuario.find({estado:true}, 'nombre email role estado google img')
+    Usuario.find({ estado: true }, 'nombre email role estado google img')
         .skip(desde)
         .limit(limite)
-        .exec((err, usuarios)=>{
+        .exec((err, usuarios) => {
 
-            if(err){
+            if (err) {
                 return res.status(400).json({
                     ok: false,
                     err
                 });
             }
 
-            Usuario.count({estado:true}, (err, conteo)=>{
-                
+            Usuario.count({ estado: true }, (err, conteo) => {
+
                 res.json({
-                ok: true,
-                usuarios,
-                cantidad: conteo
-            
+                    ok: true,
+                    usuarios,
+                    cantidad: conteo
+
                 });
             })
 
-            
+
         })
-  });
-  
-  app.post('/usuario', (req, res)=>{
-    
+});
+
+app.post('/usuario', [verificaToken, verificaAdmin_Role], (req, res) => {
+
     let body = req.body;
 
-        let usuario = new Usuario({
-            nombre:body.nombre,
-            email:body.email,
-            password: bcrypt.hashSync(body.password, 10),
-            role:body.role
+    let usuario = new Usuario({
+        nombre: body.nombre,
+        email: body.email,
+        password: bcrypt.hashSync(body.password, 10),
+        role: body.role
     });
 
-    usuario.save((err, usuarioDB)=>{
-        if(err){
+    usuario.save((err, usuarioDB) => {
+        if (err) {
             return res.status(400).json({
                 ok: false,
                 err
@@ -60,19 +61,18 @@ app.get('/usuario', function (req, res) {
         res.json({
             ok: true,
             usuario: usuarioDB
-           }
-        );
+        });
     })
 
-  });
-  
-  app.put('/usuario/:id', (req, res)=>{
-      let id = req.params.id;
-      let body = _.pick( req.body, ['nombre', 'email', 'img', 'role', 'estado']);
+});
 
-      Usuario.findByIdAndUpdate(id, body, {new: true, runValidators: true}, (err, usuarioDB)=>{
+app.put('/usuario/:id', [verificaToken, verificaAdmin_Role], (req, res) => {
+    let id = req.params.id;
+    let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
 
-        if(err){
+    Usuario.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, usuarioDB) => {
+
+        if (err) {
             return res.status(400).json({
                 ok: false,
                 err
@@ -84,12 +84,12 @@ app.get('/usuario', function (req, res) {
             usuario: usuarioDB
         });
 
-      })
-  
-  });
-  
-  app.delete('/usuario/:id', (req, res)=>{
-      
+    })
+
+});
+
+app.delete('/usuario/:id', [verificaToken, verificaAdmin_Role], (req, res) => {
+
     /*let id = req.params.id;
     Usuario.findByIdAndRemove(id, (err, usuarioBorrado) =>{
         if(err){
@@ -119,20 +119,20 @@ app.get('/usuario', function (req, res) {
     let cambiaEstado = {
         estado: false
     };
-    Usuario.findByIdAndUpdate(id, cambiaEstado, {new: true}, (err, usuarioBorrado) =>{
+    Usuario.findByIdAndUpdate(id, cambiaEstado, { new: true }, (err, usuarioBorrado) => {
 
-        if(err){
+        if (err) {
             res.status(400).json({
-                ok:false,
+                ok: false,
                 err
             });
         };
 
         res.json({
-            ok:true,
+            ok: true,
             usuario: usuarioBorrado
         });
-    } )
-  });
+    })
+});
 
-  module.exports = app;
+module.exports = app;
