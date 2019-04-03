fb50ff7662c31161305a07a0e706240aa0b0cef4
gonzalo <gonzalojvarisco@gmail.com>
2019-04-03 15:55:26 -0300

    crud categorias

--- server/routes/categoria.js
+++ server/routes/categoria.js
@@ -1,5 +1,5 @@
 const express = require('express');
-let {verificaToken} = require('../middlewares/autenticacion');
+let {verificaToken, verificaAdmin_Role} = require('../middlewares/autenticacion');
 
 const app = express();
 
@@ -12,6 +12,29 @@ const Categoria = require('../models/categoria');
 
 app.get('/categoria', (req, res)=>{
 
+    Categoria.find({}, (err, categoriaDB)=>{
+
+        if(err){
+            return res.status(500).json({
+                ok:false,
+                err
+            });
+        }
+
+        if(!categoriaDB){
+            return res.status(400).json({
+                ok:false,
+                err
+            });
+        }
+
+        res.json({
+            ok:true,
+            categorias:categoriaDB
+        });
+
+    })
+
 });
 
 //===============================
@@ -19,6 +42,30 @@ app.get('/categoria', (req, res)=>{
 //===============================
 
 app.get('/categoria/:id',(req, res)=>{
+    let id = req.params.id;
+
+    Categoria.findById(id, (err, categoriaDB)=>{
+
+        if(err){
+            return res.status(500).json({
+                ok:false,
+                err
+            });
+        }
+
+        if(!categoriaDB){
+            return res.status(400).json({
+                ok:false,
+                err
+            });
+        }
+
+        res.json({
+            ok:true,
+            categoria:categoriaDB
+        });
+
+    })
 
 });
 
@@ -28,7 +75,35 @@ app.get('/categoria/:id',(req, res)=>{
 
 app.post('/categoria', verificaToken, (req, res)=>{
    
-    
+    let body = req.body;
+
+    let categoria = new Categoria({
+        descripcion: body.descripcion,
+        usuario: req.usuario._id
+    });
+
+    categoria.save((err, categoriaDB)=>{
+        
+        if(err){
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        if(!categoriaDB){
+            return res.status(400).json({
+                ok:false,
+                err
+            });
+        }
+
+        res.json({
+            ok:true,
+            categoria:categoriaDB
+        });
+
+    });
 
 });
 
@@ -36,7 +111,33 @@ app.post('/categoria', verificaToken, (req, res)=>{
 //Actualizar categoria
 //===============================
 
-app.put('/categoria/:id', (req, res)=>{
+app.put('/categoria/:id', verificaToken, (req, res)=>{
+
+    let id = req.params.id;
+    let body = req.body;
+
+    Categoria.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, categoriaDB)=>{
+
+        if(err){
+            return res.status(500).json({
+                ok:false,
+                err
+            });
+        }
+
+        if(!categoriaDB){
+            return res.status(400).json({
+                ok:false,
+                err
+            });
+        }
+
+        res.json({
+            ok:true,
+            categoria: categoriaDB
+        });
+    })
+
 
 });
 
@@ -44,11 +145,36 @@ app.put('/categoria/:id', (req, res)=>{
 //Borrar categorias
 //===============================
 
-app.delete('/categoria/:id', (req, res)=>{
-//solo un admin puede borrar una categoria
+app.delete('/categoria/:id',[verificaToken, verificaAdmin_Role], (req, res)=>{
+
+    let id = req.params.id;
+
+    Categoria.findByIdAndRemove(id, (err, categoriaDBBorrada)=>{
+
+        if(err){
+            return res.status(500).json({
+                ok:false,
+                err
+            });
+        }
+
+        if(!categoriaDBBorrada){
+            return res.status(400).json({
+                ok:false,
+                message:'El id no existe'
+            });
+        }
+
+
+        res.json({
+            ok:true,
+            message:'Categoria borrada'
+        });
+
+    })
+
+    
 });
 
 
-module.exports = {
-    app
-}
+module.exports = app;
